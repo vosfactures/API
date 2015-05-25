721926bca09451ca61c8414be5330e491450076f
justineVF <justine@vosfactures.fr>
2015-05-25 13:15:12 +0200

    Update example1.php

--- example1.php
+++ example1.php
@@ -1,7 +1,7 @@
 <?php
 $host = 'XXXXXX.vosfactures.fr';
 $token = 'XXXXXXXXXX';
-$json ='{ "api_token": "'.$token.'", "invoice": { "kind":"vat", "number": null, "sell_date": "2013-04-17", "issue_date": "2013-04-17", "payment_to": "2013-04-24", "seller_name": "Wystawca Sp. z o.o.", "seller_tax_no": "5252445767", "buyer_name": "Klient1 Sp. z o.o.", "buyer_tax_no": "5252445767", "positions":[ {"name":"Produkt A1", "tax":23, "total_price_gross":10.23, "quantity":1}, {"name":"Produkt A2", "tax":0, "total_price_gross":50, "quantity":3} ] }}';
+$json ='{ "api_token": "'.$token.'", "invoice": { "kind":"vat", "number": null, "sell_date": "2013-04-17", "issue_date": "2013-04-17", "payment_to": "2013-04-24", "seller_name": "Société Chose", "seller_tax_no": "FR5252445767", "buyer_name": "Client Intel", "buyer_tax_no": "FR45362780010", "positions":[ {"name":"Produit A1", "tax":23, "total_price_gross":10.23, "quantity":1}, {"name":"Produit A2", "tax":0, "total_price_gross":50, "quantity":3} ] }}';
 $c = curl_init();
 curl_setopt($c, CURLOPT_URL, 'https://'.$host.'/invoices.json');
 $head[] ='Accept: application/json';
